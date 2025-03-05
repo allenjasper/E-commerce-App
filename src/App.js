5649995495cb0b0438302250481d333dc8be5741
A-C64 <angelescharles64@gmail.com>
2025-03-05 13:20:57 +0800

    adding images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import ProductList from './ProductList';
-import Cart from './Cart';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
 import './App.css';
 
 class App extends Component {
@@ -12,9 +12,9 @@ class App extends Component {
   componentDidMount() {
     // Simulating fetching product data
     const products = [
-      { id: 1, name: 'Laptop', price: 1000, image: 'https://via.placeholder.com/150' },
-      { id: 2, name: 'Phone', price: 500, image: 'https://via.placeholder.com/150' },
-      { id: 3, name: 'Headphones', price: 200, image: 'https://via.placeholder.com/150' }
+      { id: 1, name: 'Laptop', price: 1000, image: 'https://benstore.com.ph/34222-medium_default/microsoft-surface-laptop-138-touch-snapdragon-x-elite-integrated-qualcomm-adreno-16gb-lpddr5x-512gb-ssd-black-.jpg' },
+      { id: 2, name: 'Phone', price: 500, image: 'https://www.zdnet.com/a/img/resize/9f3fcf92f17d47c88823e7f2c0f1454ecd3e5140/2024/09/19/8da68e24-08b1-467a-9062-a90a96c1d879/dsc02198.jpg?auto=webp&fit=crop&height=900&width=1200' },
+      { id: 3, name: 'Headphones', price: 200, image: 'https://products.shureweb.eu/shure_product_db/product_main_images/files/c25/16a/40-/large_transparent/ce632827adec4e1842caa762f10e643d.png' }
     ];
     this.setState({ products });
   }
